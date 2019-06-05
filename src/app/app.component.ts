0bc9577cf54db13a4e554da9fdffcab941b0305b
gurezo <akihiko.kigure@gmail.com>
2019-06-05 22:42:19 +0900

    init page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { StringLenPipe } from './string-len.pipe';
 
 @Component({
   selector: 'app-root',
--- src/app/string-len.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StringLenPipe } from './string-len.pipe';
-
-describe('StringLenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StringLenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
