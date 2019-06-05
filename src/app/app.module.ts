908adf53c050ae4c54add5c063477445ad7bfd6b
gurezo <akihiko.kigure@gmail.com>
2019-06-05 23:09:24 +0900

    process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
