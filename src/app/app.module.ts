8d166161ca9b43bd395e8c55a8da09180d34e670
gurezo <akihiko.kigure@gmail.com>
2020-05-06 18:33:29 +0900

    v9 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {TranslateModule} from '@ngx-translate/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -8,7 +10,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, SharedModule, TranslateModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
