710128caa8393ac47c840fe8b9db6fd658a3de88
akihiko.kigure <akihiko.kigure@gmail.com>
2022-06-28 00:59:45 +0900

    echart refactor for breaking change

--- src/app/chart/chart.module.ts
+++ src/app/chart/chart.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgxEchartsModule } from 'ngx-echarts';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import * as echarts from "echarts";
+import { NgxEchartsModule } from "ngx-echarts";
 
-import { SharedModule } from '../shared/shared.module';
-import { ChartRoutingModule } from './chart-routing.module';
+import { SharedModule } from "../shared/shared.module";
+import { ChartRoutingModule } from "./chart-routing.module";
 
-import { ChartComponent } from './chart.component';
+import { ChartComponent } from "./chart.component";
 
 @NgModule({
-  imports: [CommonModule, ChartRoutingModule, NgxEchartsModule, SharedModule],
-  declarations: [ChartComponent]
+  imports: [
+    CommonModule,
+    ChartRoutingModule,
+    NgxEchartsModule.forRoot({ echarts }),
+    SharedModule,
+  ],
+  declarations: [ChartComponent],
 })
 export class ChartModule {}
