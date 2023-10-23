62bd8a9b02b241f30b1470cb34a73f5482abd754
akihiko.kigure <akihiko.kigure@gmail.com>
2023-10-23 18:24:03 +0900

    rebase v11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { TranslateModule } from "@ngx-translate/core";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { SharedModule } from "./shared/shared.module";
-
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    AppRoutingModule,
-    BrowserModule,
-    SharedModule,
-    TranslateModule.forRoot(),
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
